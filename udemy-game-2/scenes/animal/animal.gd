073e9da3d20133b842527cf3ac878ad2d443cb5c
Sam Gagliardi <scgagliardi2@gmail.com>
2025-01-22 20:58:30 -0600

    states

--- udemy-game-2/scenes/animal/animal.gd
+++ udemy-game-2/scenes/animal/animal.gd
@@ -4,9 +4,47 @@ extends RigidBody2D
 @onready var visible_on_screen_notifier_2d: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+enum ANIMAL_STATE { READY, DRAG, RELEASE}
+var _state: ANIMAL_STATE = ANIMAL_STATE.READY
+
+
 func _physics_process(delta: float) -> void:
-	pass
+	update(delta)
+	
+	
+func set_new_state(new_state: ANIMAL_STATE) -> void:
+	_state = new_state
+	if _state == ANIMAL_STATE.RELEASE:
+		freeze = false
+	elif _state == ANIMAL_STATE.DRAG:
+		pass
+	
+	
+func detect_release() -> bool:
+	if _state == ANIMAL_STATE.DRAG:
+		if Input.is_action_just_released("drag"):
+			set_new_state(ANIMAL_STATE.RELEASE)
+			return true
+			
+	return false
+
+
+func update_drag() -> void:
+	if detect_release():
+		return
+	
+	var gmp = get_global_mouse_position()
+	position = gmp
+	
+	
+func update(delta: float) -> void:
+	match _state:
+		ANIMAL_STATE.DRAG:
+			update_drag()
+		ANIMAL_STATE.READY:
+			pass
+		ANIMAL_STATE.RELEASE:
+			pass
 
 
 func die() -> void:
@@ -16,3 +54,8 @@ func die() -> void:
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
 	die()
+
+
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if _state == ANIMAL_STATE.READY and event.is_action_pressed("drag"):
+		set_new_state(ANIMAL_STATE.DRAG)
